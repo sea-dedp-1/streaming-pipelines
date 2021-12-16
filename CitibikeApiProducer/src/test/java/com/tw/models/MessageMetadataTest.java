3d50096bf69a30f0ec2f91de2fe8da9f67568bcd
Ryan <ryan.low@thoughtworks.com>
2021-12-16 15:04:22 +0800

    [SEA-DEDP-1-3] Add data producer for Marseille, France

--- CitibikeApiProducer/src/test/java/com/tw/models/MessageMetadataTest.java
+++ CitibikeApiProducer/src/test/java/com/tw/models/MessageMetadataTest.java
@@ -1,6 +1,5 @@
 package com.tw.models;
 
-import org.assertj.core.util.DateUtil;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -9,12 +8,12 @@ public class MessageMetadataTest {
 
     @Test
     public void shouldReturnStringWithMetadataAndPayload() {
-        long ingestionTime = DateUtil.parse("2017-01-01").getTime();
+        long ingestionUnixTime = 1483246800000L;
         String producerId = "Producer-id";
         String messageUUID = "123e4567-e89b-12d3-a456-426655440000";
         long size = 12;
 
-        MessageMetadata metadata = new MessageMetadata(ingestionTime, producerId, messageUUID, size);
+        MessageMetadata metadata = new MessageMetadata(ingestionUnixTime, producerId, messageUUID, size);
 
         String expected = "{\"producer_id\": \"Producer-id\", " +
                 "\"size\": 12, " +
