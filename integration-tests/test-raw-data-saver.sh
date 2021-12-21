3539151a87347a0419ad29a02bd0a004d7b759fb
Jun Wei Ng <junwei.ng@thoughtworks.com>
2021-12-21 11:24:40 +0800

    [SEA-DEDP-1-9][Jun Wei|Wi Su] Fix input device not TTY issue in raw data saver test script

--- integration-tests/test-raw-data-saver.sh
+++ integration-tests/test-raw-data-saver.sh
@@ -6,5 +6,6 @@ RAW_DATA_FOLDER=${1}
 
 TODAY=$(date +"%Y-%m-%d")
 
-HADOOP_LS_COMMAND="/usr/local/hadoop/bin/hadoop fs -ls /tw/rawData/${RAW_DATA_FOLDER}/data/date=${TODAY}"
-docker exec -it streamingdatapipeline_hadoop_1 ${HADOOP_LS_COMMAND} > /dev/null
+docker exec -it streamingdatapipeline_hadoop_1 \
+  /usr/local/hadoop/bin/hadoop fs -ls "/tw/rawData/${RAW_DATA_FOLDER}/data/date=${TODAY}" \
+  > /dev/null
