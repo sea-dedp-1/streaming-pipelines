9193e5121877a987c6746d3b7fc8cc8302ca4b8b
Jun Wei Ng <junwei.ng@thoughtworks.com>
2021-12-21 11:31:44 +0800

    [SEA-DEDP-1-9][Jun Wei|Wi Su] Remove interactive mode when testing raw data saver in docker

--- integration-tests/test-raw-data-saver.sh
+++ integration-tests/test-raw-data-saver.sh
@@ -6,6 +6,6 @@ RAW_DATA_FOLDER=${1}
 
 TODAY=$(date +"%Y-%m-%d")
 
-docker exec -it streamingdatapipeline_hadoop_1 \
+docker exec -t streamingdatapipeline_hadoop_1 \
   /usr/local/hadoop/bin/hadoop fs -ls "/tw/rawData/${RAW_DATA_FOLDER}/data/date=${TODAY}" \
   > /dev/null
